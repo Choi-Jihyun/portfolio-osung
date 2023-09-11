7f3bd4d2751a5708a56fd521b90db371a145d880
osung_jihyun <61964114+Choi-Jihyun@users.noreply.github.com>
2023-09-11 15:12:29 +0900

    project gsap and design

--- ts/dist/my_projects_section.js
+++ ts/dist/my_projects_section.js
@@ -1,2 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
+    var projectListWrapper = document.querySelector('.project_list');
+    var projectLi = document.querySelectorAll('.project_list > li');
+    var nextBtn = document.querySelector('.button-next');
+    var prevBtn = document.querySelector('.button-prev');
+    var slideNum = 0;
+    var inactiveColor = '#cccccc';
+    var activeColor = '#FF9900';
+    nextBtn === null || nextBtn === void 0 ? void 0 : nextBtn.addEventListener('click', goNextProject);
+    prevBtn === null || prevBtn === void 0 ? void 0 : prevBtn.addEventListener('click', goPrevProject);
+    gsap.set(prevBtn === null || prevBtn === void 0 ? void 0 : prevBtn.children, { color: inactiveColor });
+    function goNextProject() {
+        gsap.set(prevBtn === null || prevBtn === void 0 ? void 0 : prevBtn.children, { color: '#FF9900' });
+        if (slideNum < projectLi.length - 1) {
+            projectLi[slideNum].classList.remove('selected');
+            slideNum++;
+            gsap.to(projectListWrapper, { left: "-" + 28 * slideNum + "rem" });
+            projectLi[slideNum].classList.add('selected');
+        }
+        if (slideNum == projectLi.length - 1) {
+            gsap.set(nextBtn === null || nextBtn === void 0 ? void 0 : nextBtn.children, { color: inactiveColor });
+        }
+    }
+    function goPrevProject() {
+        gsap.set(nextBtn === null || nextBtn === void 0 ? void 0 : nextBtn.children, { color: '#FF9900' });
+        if (slideNum > 0) {
+            projectLi[slideNum].classList.remove('selected');
+            slideNum--;
+            gsap.to(projectListWrapper, { left: "-" + 28 * slideNum + "rem" });
+            projectLi[slideNum].classList.add('selected');
+        }
+        if (slideNum == 0) {
+            gsap.set(prevBtn === null || prevBtn === void 0 ? void 0 : prevBtn.children, { color: inactiveColor });
+        }
+    }
 });
--- ts/my_projects_section.ts
+++ ts/my_projects_section.ts
@@ -1,3 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const projectListWrapper = document.querySelector('.project_list')
+  const projectLi = document.querySelectorAll('.project_list > li')
+  const nextBtn = document.querySelector('.button-next');
+  const prevBtn = document.querySelector('.button-prev');
+  let slideNum: number = 0
+  const inactiveColor: string = '#cccccc';
+  const activeColor: string = '#FF9900';
 
+  nextBtn?.addEventListener('click', goNextProject);
+  prevBtn?.addEventListener('click', goPrevProject);
+
+  gsap.set(prevBtn?.children, {color: inactiveColor})
+
+  function goNextProject () {
+    gsap.set(prevBtn?.children, {color: '#FF9900'})
+    if (slideNum < projectLi.length - 1) {
+      projectLi[slideNum].classList.remove('selected')
+      slideNum ++;
+      gsap.to(projectListWrapper, {left: `-${28 * slideNum}rem`});
+      projectLi[slideNum].classList.add('selected')
+    }
+    if (slideNum == projectLi.length - 1) {
+      gsap.set(nextBtn?.children, {color: inactiveColor})
+    }
+  }
+  
+  function goPrevProject () {
+    gsap.set(nextBtn?.children, {color: '#FF9900'})
+    if (slideNum > 0) {
+      projectLi[slideNum].classList.remove('selected')
+      slideNum --;
+      gsap.to(projectListWrapper, {left: `-${28 * slideNum}rem`});
+      projectLi[slideNum].classList.add('selected')
+    }
+    if (slideNum == 0) {
+      gsap.set(prevBtn?.children, {color: inactiveColor})
+    }
+  }
 });
