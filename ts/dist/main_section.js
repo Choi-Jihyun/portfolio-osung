8b8ebf1dbb0c625f6d8e46269b45fbb903615cdd
osung_jihyun <61964114+Choi-Jihyun@users.noreply.github.com>
2023-09-19 11:38:45 +0900

    running osung

--- ts/dist/main_section.js
+++ ts/dist/main_section.js
@@ -2,19 +2,43 @@ document.addEventListener('DOMContentLoaded', function () {
     var runningWrapper = document.querySelector(".running_osung_wrapper");
     var osung = document.querySelector(".me");
     var backgroundPosition = 0;
-    var imgMovingLeft = -300;
+    var imgMovingLeft = 0;
+    var lastScrollY = window.scrollY;
     window.addEventListener('scroll', runningOsung);
     function runningOsung() {
-        console.log('scroll: ', scrollY);
-        backgroundPosition = backgroundPosition - 300;
-        if (backgroundPosition < -3300) {
-            backgroundPosition = 0;
+        //   console.log('scroll: ', scrollY);
+        //   backgroundPosition = backgroundPosition - 300;
+        //   if (backgroundPosition < -3300) {
+        //     backgroundPosition = 0;
+        //   }
+        //   imgMovingLeft = imgMovingLeft + 10;
+        //   if (imgMovingLeft > window.innerWidth) {
+        //     imgMovingLeft = -30;
+        //   }
+        //   osung.style.backgroundPositionX = `${backgroundPosition}px`;
+        //   runningWrapper.style.left = `${imgMovingLeft}px`;
+        // }
+        var currentScrollY = window.scrollY; // 현재 스크롤 위치
+        if (currentScrollY > lastScrollY) { // 아래로 스크롤
+            osung.style.transform = 'scaleX(1)';
+            imgMovingLeft += 15;
+            if (imgMovingLeft > window.innerWidth) {
+                imgMovingLeft = -30;
+            }
         }
-        imgMovingLeft = imgMovingLeft + 10;
-        if (imgMovingLeft > window.innerWidth) {
-            imgMovingLeft = 0;
+        else if (currentScrollY < lastScrollY) { // 위로 스크롤
+            osung.style.transform = 'scaleX(-1)';
+            imgMovingLeft -= 10;
+            if (imgMovingLeft < 0) {
+                imgMovingLeft = window.innerWidth;
+            }
+        }
+        backgroundPosition -= 300;
+        if (backgroundPosition <= -3300) {
+            backgroundPosition = 0;
         }
         osung.style.backgroundPositionX = backgroundPosition + "px";
         runningWrapper.style.left = imgMovingLeft + "px";
+        lastScrollY = currentScrollY;
     }
 });
--- ts/main_section.ts
+++ ts/main_section.ts
@@ -3,25 +3,51 @@ document.addEventListener('DOMContentLoaded', ()=>{
   const osung = document.querySelector(".me") as HTMLElement;
   
   let backgroundPosition: number = 0;
-  let imgMovingLeft: number = -300;
+  let imgMovingLeft: number = 0;
+  let lastScrollY: number = window.scrollY;
 
   window.addEventListener('scroll', runningOsung)
 
   function runningOsung() {
-    console.log('scroll: ', scrollY);
-    backgroundPosition = backgroundPosition - 300;
-    if (backgroundPosition < -3300) {
+  //   console.log('scroll: ', scrollY);
+  //   backgroundPosition = backgroundPosition - 300;
+  //   if (backgroundPosition < -3300) {
+  //     backgroundPosition = 0;
+  //   }
+  //   imgMovingLeft = imgMovingLeft + 10;
+  //   if (imgMovingLeft > window.innerWidth) {
+  //     imgMovingLeft = -30;
+  //   }
+  
+  //   osung.style.backgroundPositionX = `${backgroundPosition}px`;
+  //   runningWrapper.style.left = `${imgMovingLeft}px`;
+  // }
+
+    const currentScrollY: number = window.scrollY; // 현재 스크롤 위치
+    
+    if (currentScrollY > lastScrollY) { // 아래로 스크롤
+      osung.style.transform = 'scaleX(1)';
+      imgMovingLeft += 15;
+      if (imgMovingLeft > window.innerWidth) {
+        imgMovingLeft = -30;
+      }
+    } else if (currentScrollY < lastScrollY) { // 위로 스크롤
+      osung.style.transform = 'scaleX(-1)';
+      imgMovingLeft -=10; 
+      if(imgMovingLeft < 0){
+        imgMovingLeft= window.innerWidth ;
+      }
+    }
+    backgroundPosition -= 300;
+    if (backgroundPosition <= -3300) {
       backgroundPosition = 0;
     }
-    imgMovingLeft = imgMovingLeft + 10;
-    if (imgMovingLeft > window.innerWidth) {
-      imgMovingLeft = 0;
-    }
-  
-    osung.style.backgroundPositionX = `${backgroundPosition}px`;
-    runningWrapper.style.left = `${imgMovingLeft}px`;
+
+    osung.style.backgroundPositionX=`${backgroundPosition}px`;
+    runningWrapper.style.left=`${imgMovingLeft}px`;
+    lastScrollY=currentScrollY;
   }
 
 
 
-  })
+})
